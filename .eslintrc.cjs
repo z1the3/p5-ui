ff35f85ec73671d95cdf616abdc87f397b194d6b
z1the3 <1813783831@qq.com>
2024-01-30 13:28:11 +0800

    chore(eslint): eslint支持next目录

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'next'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -17,6 +18,10 @@ module.exports = {
   settings: {
     react: {
       version: 'detect'
+    },
+    next: {
+      rootDir: "packages/next-docs/"
     }
+
   }
 }
